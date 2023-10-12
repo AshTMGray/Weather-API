e7b66044b488cce2758e6c32d28d61598f4500a1
melissastav <melissadodwell@gmail.com>
2023-10-12 11:28:25 +0100

    'new'

---
+++ front/pages.js
@@ -0,0 +1,32 @@
+"use client";
+import ApiClient from "ClientAPI/client.js";
+//import { AuthorCard } from "@/components/AuthorCard";
+//import { useState, useEffect } from "react";
+
+export default function Home(){}
+const apiClient = new ApiClient();
+
+
+
+
+
+
+
+
+
+({date, image, description, temp}) {
+    
+    
+    unit, 
+
+    return (
+
+<div className="flex flex-row">
+    <h2 className="text-5 bold">{date}</h2>
+    <img className="" src={image}/>
+    <h3 className="">{description}</h3>
+    <p className="">{unit}</p>
+    <p className="">{temp}</p>
+</div>
+
+)}
